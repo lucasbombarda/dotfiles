fee94560f980d1223b6f96871fbf2686fc958e3f
Lucas Bombarda <lucas@devdock.com.br>
2025-02-24 07:21:33 -0300

    feat(script): fish shell to django tmux

--- scripts/django_tmux.sh
+++ scripts/django_tmux.sh
@@ -6,23 +6,36 @@ if [ -z "$1" ]; then
 fi
 cd $1 || exit 1
 
+SHELL_TYPE=$(basename $SHELL)
 PROJECT_NAME=$(basename "$1")
 
 tmux new-session -d -s $PROJECT_NAME
 
 # Editor
 tmux rename-window -t $PROJECT_NAME:1 'editor'
-tmux send-keys -t $PROJECT_NAME:1 '. venv/bin/activate' C-m
+if [ $SHELL_TYPE = "fish" ]; then
+    tmux send-keys -t $PROJECT_NAME:1 '. venv/bin/activate.fish' C-m
+else
+    tmux send-keys -t $PROJECT_NAME:1 '. venv/bin/activate' C-m
+fi
 tmux send-keys -t $PROJECT_NAME:1 'vim .' C-m
 
 # Django server
 tmux new-window -t $PROJECT_NAME:2 -n 'server'
-tmux send-keys -t $PROJECT_NAME:2 '. venv/bin/activate' C-m
+if [ $SHELL_TYPE = "fish" ]; then
+    tmux send-keys -t $PROJECT_NAME:2 '. venv/bin/activate.fish' C-m
+else
+    tmux send-keys -t $PROJECT_NAME:2 '. venv/bin/activate' C-m
+fi
 tmux send-keys -t $PROJECT_NAME:2 'python manage.py runserver' C-m
 
 # Shell
 tmux new-window -t $PROJECT_NAME:3 -n 'shell'
-tmux send-keys -t $PROJECT_NAME:3 '. venv/bin/activate' C-m
+if [ $SHELL_TYPE = "fish" ]; then
+    tmux send-keys -t $PROJECT_NAME:3 '. venv/bin/activate.fish' C-m
+else
+    tmux send-keys -t $PROJECT_NAME:3 '. venv/bin/activate' C-m
+fi
 
 tmux select-window -t $PROJECT_NAME:1
 tmux attach -t $PROJECT_NAME
