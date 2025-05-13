dbfb7b71c247adf3053a109fb096bcf12e58331b
Lucas Bombarda <lucas@devdock.com.br>
2025-05-13 11:14:04 -0300

    fix: automatic enable

--- .config/nvim/lua/lucas/lazy/lsp.lua
+++ .config/nvim/lua/lucas/lazy/lsp.lua
@@ -29,7 +29,6 @@ return {
         require("fidget").setup({})
         require("mason").setup()
         require("mason-lspconfig").setup({
-            automatic_enable = {},
             ensure_installed = {
                 "lua_ls",
                 "rust_analyzer",
