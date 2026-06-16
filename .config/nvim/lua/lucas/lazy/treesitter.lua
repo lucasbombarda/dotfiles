06f123016f673533d96fcdc7ad3bcfab87f6e2fe
Lucas Bombarda <lucas@devdock.com.br>
2026-06-16 19:38:49 -0300

    feat: bump and treesitter configs

--- .config/nvim/lua/lucas/lazy/treesitter.lua
+++ .config/nvim/lua/lucas/lazy/treesitter.lua
@@ -1,51 +1,40 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	branch = "main",
+	lazy = false,
 	build = ":TSUpdate",
-	branch = "master",
 	config = function()
-		require("nvim-treesitter.configs").setup({
-			-- A list of parser names, or "all"
-			ensure_installed = {
-				"vimdoc",
-				"javascript",
-				"typescript",
-				"c",
-				"lua",
-				"rust",
-				"jsdoc",
-				"bash",
-				"python",
-				"json",
-				"css",
-				"go",
-				"gitignore",
-				"htmldjango",
-				"html",
-				"sql",
-				"svelte",
-			},
+		local ts = require("nvim-treesitter")
 
-			indent = {
-				enable = true,
-			},
+		ts.install({
+			"vimdoc",
+			"javascript",
+			"typescript",
+			"c",
+			"lua",
+			"rust",
+			"jsdoc",
+			"bash",
+			"python",
+			"json",
+			"css",
+			"go",
+			"gitignore",
+			"htmldjango",
+			"html",
+			"sql",
+			"svelte",
+			"markdown",
+			"markdown_inline",
+		})
 
-			-- Install parsers synchronously (only applied to `ensure_installed`)
-			sync_install = false,
-
-			-- Automatically install missing parsers when entering buffer
-			-- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
-			auto_install = true,
-
-			highlight = {
-				-- `false` will disable the whole extension
-				enable = true,
-
-				-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-				-- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
-				-- Using this option may slow down your editor, and you may see some duplicate highlights.
-				-- Instead of true it can also be a list of languages
-				additional_vim_regex_highlighting = { "markdown" },
-			},
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function(ev)
+				if not pcall(vim.treesitter.start, ev.buf) then
+					return
+				end
+				vim.bo[ev.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+			end,
 		})
 	end,
 }
