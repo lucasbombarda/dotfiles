46aeec01d7c4158e78242c4c3968816a9d891051
Lucas Bombarda <lucas@devdock.com.br>
2026-07-14 16:36:53 -0300

    feat: trouble configs

--- .config/nvim/lua/lucas/lazy/trouble.lua
+++ .config/nvim/lua/lucas/lazy/trouble.lua
@@ -10,5 +10,27 @@ return {
 			"<cmd>Trouble diagnostics toggle<cr>",
 			desc = "Trouble: Toggle diagnostics",
 		},
+		{
+			"<leader>tq",
+			function()
+				local trouble = require("trouble")
+				if trouble.is_open() then
+					trouble.close()
+				else
+					trouble.open("qflist")
+				end
+			end,
+			desc = "Trouble: Fechar aberto ou abrir qflist",
+		},
+		{
+			"gd",
+			"<cmd>Trouble lsp_implementations auto_jump=true<cr>",
+			desc = "Trouble: Goto implementation",
+		},
+		{
+			"gD",
+			"<cmd>Trouble lsp_definitions auto_jump=true<cr>",
+			desc = "Trouble: Goto definition",
+		},
 	},
 }
